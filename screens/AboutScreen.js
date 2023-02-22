63f1555dd2b9979ed8d90bed52209bb8ee10a859
“Jamie <jamiedevine77@gmail.com>
2023-02-22 14:54:35 -0600

    loading

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -4,9 +4,36 @@ import { PARTNERS } from "./../shared/partners";
 import { Avatar, Card, ListItem } from "react-native-elements";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/loadingComponents";
 
 const AboutScreen = () => {
   const partners = useSelector((state) => state.partners);
+
+  if (partners.isLoading) {
+    return (
+      <ScrollView>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Loading />
+        </Card>
+      </ScrollView>
+    );
+  }
+
+  if (partners.errMess) {
+    return (
+      <ScrollView>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Text>{partners.errMess}</Text>
+        </Card>
+      </ScrollView>
+    );
+  }
 
   return (
     <ScrollView>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,14 +2,33 @@ import { ScrollView, Text, View } from "react-native";
 import { Card } from "react-native-elements";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/loadingComponents";
 
-const FeaturedItem = ({ item }) => {
+const FeaturedItem = (props) => {
+  const { item } = props;
+
+  if (props.isLoading) {
+    return <Loading />;
+  }
+  if (props.errMess) {
+    return (
+      <View>
+        <Text>{props.errMess}</Text>
+      </View>
+    );
+  }
   if (item) {
     return (
       <Card containerStyle={{ padding: 0 }}>
         <Card.Image source={{ uri: baseUrl + item.image }}>
           <View style={{ justifyContent: "center", flex: 1 }}>
-            <Text style={{ color: "white", textAlign: "center", fontSize: 20 }}>
+            <Text
+              style={{
+                color: "white",
+                textAlign: "center",
+                fontSize: 20,
+              }}
+            >
               {item.name}
             </Text>
           </View>
@@ -34,9 +53,21 @@ const HomeScreen = () => {
 
   return (
     <ScrollView>
-      <FeaturedItem item={featCampsite} />
-      <FeaturedItem item={featPromotion} />
-      <FeaturedItem item={featPartner} />
+      <FeaturedItem
+        item={featCampsite}
+        isLoading={campsites.isLoading}
+        errMess={campsites.errMess}
+      />
+      <FeaturedItem
+        item={featPromotion}
+        isLoading={promotions.isLoading}
+        errMess={promotions.errMess}
+      />
+      <FeaturedItem
+        item={featPartner}
+        isLoading={partners.isLoading}
+        errMess={partners.errMess}
+      />
     </ScrollView>
   );
 };
