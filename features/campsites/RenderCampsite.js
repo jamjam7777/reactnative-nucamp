d14a8e02adef083c8888e08d8e8e03876f94067b
“Jamie <jamiedevine77@gmail.com>
2023-03-01 14:38:20 -0600

    bounce

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View, PanResponder, Alert } from "react-native";
+import { useRef } from "react";
 import { Card, Icon } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
 import * as Animatable from "react-native-animatable";
@@ -6,10 +7,17 @@ import * as Animatable from "react-native-animatable";
 const RenderCampsite = (props) => {
   const { campsite } = props;
 
+  const view = useRef();
+
   const isLeftSwipe = ({ dx }) => dx < -200;
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      view.current
+          .rubberBand(1000)
+          .then((endState) => console.log(endState.finished? 'finished' : 'canceled'))
+    },
     onPanResponderEnd: (e, gestureState) => {
       console.log('pan responder end', gestureState);
       if (isLeftSwipe(gestureState)) {
@@ -39,6 +47,7 @@ const RenderCampsite = (props) => {
   if (campsite) {
     return (
       <Animatable.View animation="fadeInDownBig" duration={2000} delay={1000}
+      ref={view}
       {...panResponder.panHandlers}
       >
         <Card containerStyle={styles.cardContainer}>
